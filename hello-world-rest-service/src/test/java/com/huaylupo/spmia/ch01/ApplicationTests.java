d39557125bf86f1977cad30911f87ae7b563be26
ggaube <78953778+ggaube@users.noreply.github.com>
2024-02-28 11:08:03 +0100

    Add files via upload

---
+++ hello-world-rest-service/src/test/java/com/huaylupo/spmia/ch01/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huaylupo.spmia.ch01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
